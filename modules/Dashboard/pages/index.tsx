9a86d362ae285227f83edd61cc45119048fbfe8e
Eduardo Felipe Araya <eduardojezine@gmail.com>
2022-01-24 17:49:43 -0300

    [feature]: Mock the chart in dashboard page

--- modules/Dashboard/pages/index.tsx
+++ modules/Dashboard/pages/index.tsx
@@ -1,7 +1,7 @@
+import { Box, Grid, Typography } from "@mui/material";
+import Pie from "modules/Chart/components/Pie";
 import Layout from "../../Common/components/Layout";
-import React from "react";
 import PaperInfo from "../../Common/components/PaperInfo";
-import { Grid } from "@mui/material";
 
 export interface DashboardPageProps {}
 
@@ -20,6 +20,30 @@ export const DashboardPage: React.FC<DashboardPageProps> = () => {
         </Grid>
         <Grid item lg={3} flex={1}>
           <PaperInfo title="Investimentos" value="R$ 1.000.000,00" />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{ padding: "35px 10px" }}
+        justifyContent={"space-around"}
+      >
+        <Grid item lg={4}>
+          <Typography>Gastos por categoria</Typography>
+          <Box sx={{ maxWidth: "360px" }}>
+            <Pie />
+          </Box>
+        </Grid>
+        <Grid item lg={4}>
+          <Typography>Carteira</Typography>
+          <Box sx={{ maxWidth: "360px" }}>
+            <Pie />
+          </Box>
+        </Grid>
+        <Grid item lg={4}>
+          <Typography>Entradas</Typography>
+          <Box sx={{ maxWidth: "360px" }}>
+            <Pie />
+          </Box>
         </Grid>
       </Grid>
     </Layout>
