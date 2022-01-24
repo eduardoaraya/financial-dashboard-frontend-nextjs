8202f53d23a9c4bdad9594a7bc2224f0d71b6cf2
Eduardo Felipe Araya <eduardojezine@gmail.com>
2022-01-24 17:49:21 -0300

    [feature]: Add Chart module

---
+++ modules/Chart/components/Pie/index.tsx
@@ -0,0 +1,41 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie as PieChart } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip);
+
+export const defaultData = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export interface PieProps {
+  data?: any;
+}
+
+export const Pie: React.FC<PieProps> = ({ data = defaultData }) => (
+  <PieChart data={data} />
+);
+
+export default Pie;
