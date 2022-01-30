2166a9e123eb70915286ed7ab8ff9cf94e3a0718
Eduardo Felipe Araya <eduardojezine@gmail.com>
2022-01-30 01:15:03 -0300

    [fix]: Remove console.log

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,15 +8,10 @@ module.exports = {
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: "@storybook/react",
   webpackFinal: async (config) => {
-    // config.resolve.modules = [
-    //   ...(config.resolve.modules || []),
-    //   path.resolve(__dirname, "..", "libs", "src"),
-    // ];
     config.resolve.alias = {
       ...config.resolve.alias,
       "@core": path.resolve(__dirname, "..", "libs", "src"),
     };
-    console.log("aq");
     return config;
   },
 };
